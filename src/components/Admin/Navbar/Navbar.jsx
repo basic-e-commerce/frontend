820d3d83a934eea200ff3807e2c50ca0dce1a352
Ömer Türkay <omerturkay@192.168.1.105>
2025-08-13 15:05:49 +0300

    asdgasdg

--- src/components/Admin/Navbar/Navbar.jsx
+++ src/components/Admin/Navbar/Navbar.jsx
@@ -1,28 +1,34 @@
 import "./Navbar.scss";
 import MenuIcon from "@mui/icons-material/Menu";
 import Avatar from "@mui/material/Avatar";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
         <div className="navbarWrapper">
-          <div className="navbarLeft">
-            <button style={{ borderRadius: "20px" }}>
-              <MenuIcon
-                style={{ color: "rgb(24, 20, 36)" }}
-                className="iconn"
-              />
-            </button>
-          </div>
-
           <div className="navbarRight">
             <div className="avatarTamplate">
-              <Avatar alt="Ömer Türkay  " src="/images/userImg/1.jpg" />
-              <span>Ömer Türkay</span>
-              <ArrowDropDownIcon />
+              <Avatar alt="Gülce Gıda" />
+              <span>Gülce Gıda</span>
             </div>
+          </div>
+
+          <div className="navbarLeft">
+            {/*<button
+              style={{
+                background: "transparent",
+                borderRadius: "20px",
+              }}
+            >
+              <MenuIcon
+                style={{
+                  background: "transparent",
+                  color: "rgb(24, 20, 36)",
+                }}
+                className="iconn"
+              />
+            </button> */}
           </div>
         </div>
       </div>
